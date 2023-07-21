1202177d02960c3188af66e77da85bcd645cb0a8
izawa-ry <izawa-ry@ibjapan.jp>
2023-07-21 20:29:52 +0900

    [Modify]YWT対応

--- write_memo.sh
+++ write_memo.sh
@@ -1,21 +1,20 @@
-#!/bin/zsh
+#!/bin/sh
 
-if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+YEAR=$(date +%Y)
+MONTH=$(date +%Y%m)
+MEMO_HOME="$HOME/memo/$YEAR/$MONTH"
+MEMO_FILE="$MEMO_HOME/$(date +%Y_%m_%d).txt"
 
- writeMemo() {
-   YEAR=$(date +%Y)
-   MONTH=$(date +%Y%m)
-   MEMO_HOME="$HOME/memo/$YEAR/$MONTH"
-   MEMO_FILE="$MEMO_HOME/$(date +%Y_%m_%d).txt"
-
-   if [[ ! -d $MEMO_HOME ]]; then
-     mkdir -p "$MEMO_HOME"
-   fi
-   if [[ ! -f $MEMO_FILE ]]; then
-     touch "$MEMO_FILE"
-   fi
-   vim "$MEMO_FILE"
- }
- writeMemo "$@"
-
+if [[ ! -d $MEMO_HOME ]]; then
+mkdir -p "$MEMO_HOME"
 fi
+if [[ ! -f $MEMO_FILE ]]; then
+echo "=========================" >> "$MEMO_FILE"
+echo "やったこと" >> "$MEMO_FILE"
+echo "" >> "$MEMO_FILE"
+echo "わかったこと" >> "$MEMO_FILE"
+echo "" >> "$MEMO_FILE"
+echo "次やること" >> "$MEMO_FILE"
+echo "=========================" >> "$MEMO_FILE"
+fi
+vim "$MEMO_FILE"
